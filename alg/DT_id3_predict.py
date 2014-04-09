f0a8b2e518ebbc13e8176b21decd78686c728709
wchgit <wchgeek@gmail.com>
2014-04-09 23:13:33 +0800

    modified several variable name for better understanding.

--- alg/DT_id3_predict.py
+++ alg/DT_id3_predict.py
@@ -6,12 +6,12 @@ db = DB('train.db')
 n_feature = int(db.queryone('value','manifest','key="n_feature"')[0])
 
 fin = open('model.dat')
-neigh = pickle.load(fin)
+clf = pickle.load(fin)
 fin.close
 
 def predict(dic):
     data = [-100]*n_feature
     for m_id, rss in dic.items():
         data[int(m_id)] = float(rss)
-    cls = neigh.predict([data])
+    cls = clf.predict([data])
     return cls[0]
